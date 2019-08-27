423a66cc5f3453ed0f2a400e3c4c20d594cd4f43
Raymond Gonzalez <raymondalexander.gonzalez@gmail.com>
2019-08-27 00:23:21 +0000

    Done.

--- calculator.rb
+++ calculator.rb
@@ -1,9 +1,7 @@
-def local_variables do
 first_number = 4
 second_number = 4
-end
-
 sum = first_number + second_number
 difference = first_number - second_number
 product = first_number * second_number
 quotient = first_number / second_number
+
